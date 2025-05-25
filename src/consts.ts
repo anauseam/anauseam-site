bc3d2af4343aeee73a39346cfb7d9137a4f49131
Anauseam <cam@anauseam.org>
2025-05-25 00:26:01 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Anauseam";
+export const SITE_DESCRIPTION = "Anauseam landing page";
